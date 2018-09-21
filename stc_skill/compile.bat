49822fefbd3af3c872818de04dcb7510bacfe2b7
Alex Ayala <alexayala9975@hotmail.com>
2018-09-20 17:47:57 -0700

    Correct spelling of lambda

--- stc_skill/compile.bat
+++ stc_skill/compile.bat
@@ -4,4 +4,4 @@ RMDIR /S /Q skill_env
 MKDIR skill_env
 
 pip install -r requirements.txt -t skill_env
-xcopy /s lamda skill_env
+xcopy /s lambda skill_env
--- stc_skill/compile.sh
+++ stc_skill/compile.sh
@@ -4,4 +4,4 @@ rm -R skill_env
 mkdir skill_env
 
 pip install -r requirements.txt -t skill_env
-cp lamda/* skill_env/
+cp lambda/* skill_env/
