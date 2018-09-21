87ef536bf2c4b9dc61cd70dd377efc13db27d392
Alex Ayala <alexayala9975@hotmail.com>
2018-09-20 17:47:22 -0700

    Correct spelling of lambda

--- stc_skill/lambda/lambda_function.py
+++ stc_skill/lambda/lambda_function.py
@@ -21,6 +21,17 @@ class LaunchRequestHandler(AbstractRequestHandler):
         return handler_input.response_builder.response
 
 
+class HelloRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_request_type("HelloIntent")(handler_input)
+    
+    def handle(self, handler_input):
+        speech = "Hello!"
+        print("Alexa Request: {}\n".format(handler_input.request_envelope.request))
+        handler_input.response_builder.speak(speech)
+        return handler_input.response_builder.response
+
 sb.add_request_handler(LaunchRequestHandler())
+sb.add_exception_handler(HelloRequestHandler())
 
 lambda_handler = sb.lambda_handler()
